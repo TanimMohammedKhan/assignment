2401398b122bee4f1b08468e80957fdb0b20e234
Tanimm27 <ktanim647@gmail.com>
2025-08-20 20:27:52 +0600

    articlepage added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import ProfilePage from './pages/Profilepage';
+import ArticlesPage from "./pages/ArticlesPage";
 import "./App.css";
 
 function App() {
@@ -13,9 +14,10 @@ function App() {
 
       <main className="content">
         <Routes>
-          <Route path="/" element={<Navigate to="/profile" replace />} />
-          <Route path="/profile" element={<ProfilePage />} />       
-          <Route path="*" element={<Navigate to="/profile" replace />} />
+          <Route path="/" element={<Navigate to="/articles" replace />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="*" element={<Navigate to="/articles" replace />} />
         </Routes>
       </main>
     </div>
